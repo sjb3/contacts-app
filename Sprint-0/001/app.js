231d45db952a67ccbf56a51bb61f4192e179e2d2
Ryan Ewing <ryan.ewing@concur.com>
2017-02-11 11:46:09 -0800

    small tweak for comment ordering

--- Sprint-0/001/app.js
+++ Sprint-0/001/app.js
@@ -1,11 +1,11 @@
-// 3/ Using pure JavaScript we use createElement to create a representation of our DOM node
-// component is either a DOM component or Custom Component
-//-- React.createElement(component, [props], [...children])
 
-// Using ReactDOM, we insert our representation, called a "react element", into our DOM tree
+// 3/ 2. Using ReactDOM, we insert our representation, called a "react element", into our DOM tree
 // ReactDOM.render(element, container, [callback])
 
 ReactDOM.render(
+// 3/ 1. Using pure JavaScript we use createElement to create a representation of our DOM node
+// component is either a DOM component or Custom Component
+//-- React.createElement(component, [props], [...children])
   React.createElement('div', {}, 'Albert Einstein'),
   document.getElementById('root')
 )
